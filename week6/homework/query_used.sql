a7f98d8bcefa58dd9aa13cbf9934e4d8d9dedd7a
A.Kramer <voitel@gmail.com>
2016-02-16 19:46:39 -0800

    Added homework 6 files

---
+++ week6/homework/query_used.sql
@@ -0,0 +1,13 @@
+﻿select dell_store.customers.customerid, dell_store.customers.firstname,
+       dell_store.customers.lastname, dell_store.orders.orderid,
+       dell_store.orderlines.quantity, dell_store.products.title,
+       dell_store.products.price, dell_store.categories.categoryname
+    from dell_store.customers 
+left join dell_store.orders 
+    on dell_store.customers.customerid = dell_store.orders.customerid
+left join dell_store.orderlines
+    on dell_store.orders.orderid = dell_store.orderlines.orderid
+left join dell_store.products
+    on dell_store.orderlines.prod_id = dell_store.products.prod_id
+left join dell_store.categories
+    on dell_store.products.category = dell_store.categories.category
